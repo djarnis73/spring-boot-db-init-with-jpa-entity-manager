d17036e981f84b6ff1d2955ce0d1e6441cc64922
Jens Teglhus Møller <ekjetm@danskespil.dk>
2019-07-04 13:54:45 +0200

    Use ApplicationStartedEvent instead of ApplicationReadyEvent That event happens a little earlier. I also tried some of the other event type, but they do not seem to work (as in the init method is never called).

--- src/main/java/example/Initializer.java
+++ src/main/java/example/Initializer.java
@@ -1,6 +1,8 @@
 package example;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.SpringApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,14 +11,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Component
+@Slf4j
 public class Initializer {
 
     @PersistenceContext
     EntityManager em;
 
-    @EventListener(ApplicationReadyEvent.class)
+    @EventListener(ApplicationStartedEvent.class)
     @Transactional
-    public void init() {
+    public void init(SpringApplicationEvent event) {
+        log.info("Initializing db: {}", event);
         for (int i = 0; i < 10; i++) {
             em.merge(new MyEntity());
         }
